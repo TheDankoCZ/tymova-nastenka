7c7a7b61accb343ea576d1ba0c5d62eb16278009
Bezdek <stepan.bezdek@email.cz>
2024-02-11 01:43:50 +0100

    Vytváření nových lístečků, zobrazování z databáze, upravování lístečků + ukládání pozice lístečku na nástěnce (puze nástřel)

--- app/nastenka/Index.php
+++ app/nastenka/Index.php
@@ -10,6 +10,14 @@ class Index
     {
         $base->set("title","Nástěnka");
         $base->set("content","nastenka.html");
+
+        //$uzivatel_id = $base->get('SESSION.user[id]');
+        $uzivatel_id = 1;
+        $listecky = new Listecky;
+        $data = $listecky->find(['autor=?',$uzivatel_id]);
+
+        $base->set("data", $data);
+
         echo \Template::instance()->render("index.html");
     }
 
@@ -24,13 +32,38 @@ class Index
     {
         $listecek = new Listecky();
         //nemám přehled jak je udělaná SESSION tak to pak bude potřeba opravit
-        $listecek->author = $base->get('SESSION.user[id]');
+        //$listecek->author = $base->get('SESSION.user[id]');
+        $listecek->author = 1;
         $listecek->copyfrom($base->get("POST"));
         $listecek->save();
         $base->reroute('/');
     }
+    public function post_zmena_pozice(\Base $base)
+    {
+        //ok Evidentně neumím pracovat s JSON
+        //TODO Nevím jak na to tak se na to pls někdo mrkněte, konzole tvrdí že data to posílá správný
+        $list = new Listecky();
+        $listecek = $list->load(["id=?",$base->get('POST.id')]);
+        $listecek->x = $base->get('POST.x');
+        $listecek->y = $base->get('POST.y');
+        $listecek->save();
+    }
     public function get_upravit(\Base $base)
     {
+        $base->set("title","Nový lístek");
+        $base->set("content","upravit_list.html");
+        $listecek = new Listecky();
+        $data = $listecek->findone(["id=?",$base->get('PARAMS.id')]);
+        $base->set("data", $data);
+        echo \Template::instance()->render("index.html");
+    }
 
+    public function post_upravit(\Base $base)
+    {
+        $listecek = new Listecky();
+        $listecek->load(['id=?', $base->get('PARAMS.id')]);
+        $listecek->copyfrom($base->get("POST"));
+        $listecek->save();
+        $base->reroute('/');
     }
 }
